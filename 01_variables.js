ae060895d18abf27ef896e0e73e39b083d8f9710
SAVAGE-Varshan <varshansharma7@gmail.com>
2026-02-08 17:16:59 +0000

    variables 01

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14233526
+let accountEmail = "Varshan@github.com"
+var accountName = "Varshan"
+accountnumber = 1234
+let accountstate;
+
+console.log(accountId)
+// accountId = 763 // not to be used
+accountEmail = "varsh"
+accountName = "github"
+accountnumber = 7777
+console.table([accountEmail,accountName,accountId,accountstate,accountnumber])
