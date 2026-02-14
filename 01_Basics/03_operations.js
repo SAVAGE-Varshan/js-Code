21f06797d7516e47fb045b1be4c8d6b7114ac45f
SAVAGE-Varshan <varshansharma7@gmail.com>
2026-02-14 11:39:18 +0000

    operations

---
+++ 01_Basics/03_operations.js
@@ -0,0 +1,15 @@
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+
+console.log(+true)
+// console.log(true+) // Error
+
+console.log(+"");
+
+let gameCounter = 100;
+++gameCounter;
+gameCounter++;
+console.log(gameCounter);
